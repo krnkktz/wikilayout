ff5f3a6e8f67bbcf6640534707154fab26692a64
Marin Roche <marinroche@gmail.com>
2019-06-29 18:52:00 +0200

    further fixes

--- ext.js
+++ ext.js
@@ -1,13 +1,28 @@
+/* remove all inline css, breaks a lot of stuff.. */
 /*document.querySelectorAll('[style]').forEach(el => el.removeAttribute('style')); */
+
+/* remove inline css for font-size */
+document.querySelectorAll('[style]').forEach(el => {
+    if (el.style['font-size']) {
+        el.style['font-size'] = '';
+    }
+});
+
+/* remove linked stylesheets */
 document.querySelectorAll('link[rel="stylesheet"]').forEach(el => el.parentNode.removeChild(el));
+
+/* remove internal css */
 document.querySelectorAll('style').forEach(el => el.parentNode.removeChild(el));
+
+/* inject our own css */
 var style = document.createElement("STYLE");
 var text = document.createTextNode(css);
 style.appendChild(text);
 document.head.appendChild(style);
 
 
-/* if i'm not mistaken, this takes ~4x more internet, so don't do that on
+/* using directly the x2 images sizes
+ * if i'm not mistaken, this takes ~4x more internet, so don't do that on
  * mobile. */
 document.querySelectorAll('.thumbinner').forEach(el => { el.style['min-width'] = el.style['width']; el.style['width'] = '20vw'; });
 
@@ -18,4 +33,4 @@ document.querySelectorAll('.thumbimage').forEach(el => {
     el.style['width'] = '20vw';
     el.style['height'] = 'auto';
     el.src = el.srcset.split(',')[1].split(' ')[1];
-    el.width = null, el.height = null;});
+    el.width = undefined, el.height = undefined;});
