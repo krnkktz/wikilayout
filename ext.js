a9a36bed5201bef6a04d8497b609bd2923f4dcae
Marin Roche <marinroche@gmail.com>
2019-06-30 20:25:24 +0200

    centered infoboxes and thumbnails for narrower viewports

--- ext.js
+++ ext.js
@@ -14,23 +14,31 @@ document.querySelectorAll('link[rel="stylesheet"]').forEach(el => el.parentNode.
 /* remove internal css */
 document.querySelectorAll('style').forEach(el => el.parentNode.removeChild(el));
 
+
+/* using directly the x2 images sizes
+ * if i'm not mistaken, this takes ~4x more internet, so don't do that on mobile data? though we're not there yet... */
+document.querySelectorAll('.thumbinner').forEach(el => { el.style['min-width'] = el.style['width']; el.style['width'] = ''; });
+
+
+document.querySelectorAll('.thumbimage').forEach(el => {
+    el.style['min-width'] = el.width + 'px';
+    el.style['max-width'] = el.width * 2 + 'px';
+    el.style['height'] = 'auto';
+    el.srcset.split(',').forEach(e => {
+        var e1 = e.trim();
+        if (e1.endsWith('2x')) {
+            el.src = e.split(' ')[0];
+        }
+    });
+    el.width = undefined;
+    el.height = undefined;});
+
+
 /* inject our own css */
 var style = document.createElement("STYLE");
 var text = document.createTextNode(css);
 style.appendChild(text);
 document.head.appendChild(style);
 
-
-/* using directly the x2 images sizes
- * if i'm not mistaken, this takes ~4x more internet, so don't do that on
- * mobile. */
-document.querySelectorAll('.thumbinner').forEach(el => { el.style['min-width'] = el.style['width']; el.style['width'] = '20vw'; });
-
-document.querySelectorAll('.thumbimage').forEach(el => {
-    max_factor = 2; // is it sometimes different?
-    el.style['min-width'] = el.width + 'px';
-    el.style['max-width'] = el.width * max_factor + 'px';
-    el.style['width'] = '20vw';
-    el.style['height'] = 'auto';
-    el.src = el.srcset.split(',')[1].split(' ')[1];
-    el.width = undefined, el.height = undefined;});
+/* and this is where the browser should start querying other stuff,
+ * interpreting css and running scripts.. */
