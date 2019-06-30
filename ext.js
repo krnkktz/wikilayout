c155063822880fd3b8e8326af93cff73d26528ef
Marin Roche <marinroche@gmail.com>
2019-06-30 22:15:16 +0200

    increase tnone resolution too

--- ext.js
+++ ext.js
@@ -20,23 +20,36 @@ document.querySelectorAll('style').forEach(el => el.parentNode.removeChild(el));
  * if i'm not mistaken, this takes ~4x more internet, so don't do that on
  * mobile data? though we're not there yet... ideally, just don't do that if
  * 80vw is <= image width */
-document.querySelectorAll('.tnone .thumbinner').forEach(el => { el.style['max-width'] = el.style['width']; el.style['width'] = ''; });
+
+function get2x(src, srcset) {
+    l = srcset.split(',');
+    for (var i = 0; i < l.length; i++) {
+        var e1 = l[i].trim();
+        if (e1.endsWith('2x')) {
+            return e1.split(' ')[0];
+        }
+    }
+    return src;
+}
 
 
 document.querySelectorAll('.tleft .thumbimage, .tright .thumbimage').forEach(el => {
     var ti = el.parentElement.parentElement;
     el.style['min-width'] = el.width + 'px';
     el.style['max-width'] = el.width * 2 + 'px';
-    ti.style['min-width'] = el.style['width'];
+    ti.style['min-width'] = el.width + 'px';
     ti.style['max-width'] = el.width * 2 + 'px';
     ti.style['width'] = '';
-    el.srcset.split(',').forEach(e => {
-        var e1 = e.trim();
-        if (e1.endsWith('2x')) {
-            el.src = e.split(' ')[0];
-        }
-    });
-    ;});
+    el.src = get2x(el.src, el.srcset);
+});
+
+document.querySelectorAll('.tnone .thumbimage').forEach(el => {
+    var ti = el.parentElement.parentElement;
+    el.style['max-width'] = el.width * 2 + 'px';
+    ti.style['max-width'] = el.width * 2 + 'px';
+    ti.style['width'] = '';
+    el.src = get2x(el.src, el.srcset);
+});
 
 
 /* inject our own css */
